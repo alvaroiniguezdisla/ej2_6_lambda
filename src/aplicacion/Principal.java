53c22bdc566bd82a2cfc485f22180d059f9bf62a
Álvaro <ainiguezdislaa@gmail.com>
2023-02-21 16:03:32 +0100

    ej terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,10 @@
-package aplicacion;public class Principal {
+package aplicacion;
+
+public class Principal {
+    public static void main(String[] args) {
+        int[] lista = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        System.out.println("Media: " + dominio.ej2_6_lambda.media(lista));
+        System.out.println("Cuadrados: " + dominio.ej2_6_lambda.cuadrados(lista));
+        System.out.println("Desviación: " + dominio.ej2_6_lambda.desviacion(lista));
+    }
 }
--- src/dominio/ej2_6_lambda.java
+++ src/dominio/ej2_6_lambda.java
@@ -1,2 +1,17 @@
-package dominio;public class ej2_6_lambda {
+package dominio;
+import java.util.stream.Stream;
+
+public class ej2_6_lambda  {
+    public static int media(int[]lista){
+        return Stream.iterate(0,i->i+1).limit(lista.length).map(i->lista[i]).reduce(0,(a,b)->a+b)/lista.length;
+    }
+    public static int cuadrados(int[]lista){
+        return Stream.iterate(0,i->i+1).limit(lista.length).map(i->lista[i]).reduce(0,(a,b)->a+b*b);
+    }
+
+    public static double desviacion(int [] lista){
+        double media=media(lista);
+        double cuadrados=cuadrados(lista);
+        return Math.sqrt((cuadrados/lista.length)-(media*media));
+    }
 }
